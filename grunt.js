19e285674de552f923613c8f51798df0a63d6e60
Ladislav Prskavec <ladislav@prskavec.net>
2012-08-20 20:16:39 +0200

    Add grunt compass

--- grunt.js
+++ grunt.js
@@ -1,7 +1,31 @@
 module.exports = function(grunt) {
+  // Load tasks
   grunt.loadNpmTasks('grunt-coffeelint');
+  grunt.loadNpmTasks('grunt-compass');
+  grunt.loadNpmTasks('grunt-heroku-deploy');
   // Project configuration.
   grunt.initConfig({
+    compass: {
+      dev: {
+        src: 'sass',
+        dest: 'public/stylesheets',
+        outputstyle: 'expanded',
+        linecomments: true
+      },
+      prod: {
+        src: 'sass',
+        dest: 'public/stylesheets',
+        outputstyle: 'compressed',
+        linecomments: false,
+        forcecompile: true
+      }
+    },
+    watch: { // for development run 'grunt watch'
+      compass: {
+        files: ['sass/*.sass'],
+        tasks: ['compass:dev']
+      }
+    },
     coffeelint: {
       one: {
         files: ['app.coffee','lib/*.coffee'],
